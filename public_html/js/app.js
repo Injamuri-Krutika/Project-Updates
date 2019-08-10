d0e233aef33d4669ad63680b1cd64d01ddea553f
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-08-10 11:38:55 +0530

    Remove diable class for nav item, Update Routes

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -5,8 +5,9 @@
         var router = new Router([
             new Route('home', 'home.html', true),
             new Route('papers', 'papers.html'),
-            new Route('register', 'register.html'),
             new Route('code-references', 'codereferences.html'),
+            new Route('tips-and-tricks', 'tipsandtricks.html'),
+
         ]);
     }
     init();
