8610de22d8fe3dccdcce2728ab77e0428968f11a
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-08-10 11:32:50 +0530

    Update the routes

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -4,20 +4,9 @@
     function init() {
         var router = new Router([
             new Route('home', 'home.html', true),
-            new Route('login', 'login.html'),
+            new Route('papers', 'papers.html'),
             new Route('register', 'register.html'),
-
-            new Route('contact', 'contact.html'),
-            new Route('placement', 'placements.html'),
-            new Route('latestnews', 'subpages/activities/latestnews.html'),
-            new Route('events', 'subpages/activities/events.html'),
-            new Route('faculty', 'subpages/people/faculty.html'),
-            new Route('visitors', 'subpages/people/visitors.html'),
-            new Route('admissions', 'subpages/academics/admissions.html'),
-            new Route('programs', 'subpages/academics/programs.html'),
-            new Route('syllabus', 'subpages/academics/syllabus.html'),
-
-
+            new Route('code-references', 'codereferences.html'),
         ]);
     }
     init();
