13a567a41d07744be1387d5be6298a343129b575
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2019-08-20 12:07:11 +0530

    Add Progress Tab to record the research progress

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -5,7 +5,7 @@
         var router = new Router([
             new Route('home', 'home.html', true),
             new Route('papers', 'papers.html'),
-            new Route('code-references', 'code-references.html'),
+            new Route('progress', 'progress.html'),
             new Route('tips-tricks', 'tips-tricks.html')
         ]);
     }
